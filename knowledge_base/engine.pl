5bd526fd69f7ec1c1d75f41ae40ac5ff93611278
Rafał Franczak <franczakr066@gmail.com>
2021-11-23 16:14:41 +0100

    ostatni commit dzis byczki, bo wiecej mi sie nie chce

--- knowledge_base/engine.pl
+++ knowledge_base/engine.pl
@@ -6,4 +6,8 @@ recipe("bigos", ["kiełbasa", "kapusta"]).
 recipe("bigos ale lepszy", ["kiełbasa", "kapusta", "sól"]).
 recipe("bigos ale bez kiełbasy", ["kapusta"]).
 
+:- dynamic(custom_recipe/2).
+
+recipe(X, Y) :- custom_recipe(X, Y).
+
 get_recipes(X, Ingredients) :- recipe(X, Required), has_all_ingredients(Required, Ingredients).
--- src/message_processor.py
+++ src/message_processor.py
@@ -8,22 +8,38 @@ class MessageProcessor:
         self.prolog.consult('knowledge_base/engine.pl')
 
     async def parse(self, message_object, client) -> (bool, str):
-        products = self.parse_products(message_object.content)
-        recipes = self.get_recipes(products)
-        response = self.prepare_response(recipes)
-        return True, response
+        message = message_object.content
+        response = ""
+        if message.startswith('+przepis'):
+            response = self.add_recipe(message[len('+przepis'):])
+        elif message == 'lista':
+            response = self.list_recipes()
+        elif message.startswith('?przepis'):
+            response = self.get_recipes(message[len('?przepis'):])
+        return True if response != "" else False, response
 
-    def parse_products(self, message):
-        products = message.split(':')[1]
-        products = [product.strip().lower() for product in re.split(r"\s*[,.]+\s*", products)]
-        return products
+    def add_recipe(self, message):
+        dish_name, products = message.split(':', 1)
+        dish_name = dish_name.strip().lower()
+        products = self.parse_products(products)
+        self.prolog.assertz(f'custom_recipe("{dish_name}", {products})')
+        return f"(Może) dodano przepis {dish_name}: {products}"
 
-    def get_recipes(self, products):
-        products = [f'"{product}"' for product in products]
-        products = "[" + ', '.join(products) + "]"
-        return [output['X'].decode("utf-8") for output in self.prolog.query(f'get_recipes(X, {products})')]
-
-    def prepare_response(self, recipes):
+    def get_recipes(self, message):
+        products = self.parse_products(message)
+        recipes = [output['X'] for output in self.prolog.query(f'get_recipes(X, {products})')]
+        recipes = [recipe if isinstance(recipe, str) else recipe.decode("utf-8") for recipe in recipes]
         if recipes is None or len(recipes) == 0:
             return "Co Pan zrobisz? Nic Pan z tego nie zrobisz"
         return "Dostępne przepisy:" + "".join([f'\n\t- {recipe}' for recipe in recipes])
+
+    def list_recipes(self):
+        recipes = [output['X'] for output in self.prolog.query(f'get_recipes(X, Y)')]
+        recipes = [recipe if isinstance(recipe, str) else recipe.decode("utf-8") for recipe in recipes]
+        return "Lista wszystkich przepisów:" + "".join([f'\n\t- {recipe}' for recipe in recipes])
+
+    def parse_products(self, products):
+        products = [product.strip().lower() for product in re.split(r"\s*[,.]+\s*", products)]
+        products = [f'"{product}"' for product in products]
+        products = "[" + ', '.join(products) + "]"
+        return products
