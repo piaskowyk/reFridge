82207e3ed043a8f88d442473f57bbb223f762322
Rafał Franczak <franczakr066@gmail.com>
2021-12-05 12:09:10 +0100

    dalej nie wiem co robie bo krzychu mi nie sprecyzowal co zrobic

--- knowledge_base/engine.pl
+++ knowledge_base/engine.pl
@@ -1,13 +1,52 @@
-has_all_ingredients(RequiredIngredients, Ingredients) :-
-    forall(member(Element, RequiredIngredients), member(Element, Ingredients)).
+:- dynamic(ingredients/1).
+
+has_all_ingredients(RequiredIngredients) :-
+    forall(member(Element, RequiredIngredients), all_ingredients(Element)).
+
+mieso_count(Count) :- !,
+        findall(X, (mieso(X), all_ingredients(X)), L),
+        length(L, Count).
+
+mieso("wołowina") .
+mieso("wieprzowina"). 
+mieso("drób").
+
+all_ingredients(X) :- ingredients(X).
+
+all_ingredients("mięso mielone") :- all_ingredients("wołowina"); all_ingredients("wieprzowina").
+
+all_ingredients("wołowina") :- all_ingredients("karkówka wołowa"); all_ingredients("łopatka wołowa").
+all_ingredients("wieprzowina") :- all_ingredients("schab"); all_ingredients("polędwiczka"); all_ingredients("szynka").
+all_ingredients("drób") :- all_ingredients("udka"); all_ingredients("skrzydełka"); all_ingredients("pierś z kurczaka").
+
+fruits_count(Count) :- !,
+        findall(X, (fruit(X), all_ingredients(X)), L),
+        length(L, Count).
+
+fruit("jabłko").
+fruit("truskawka").
+fruit("banan").
+
+vegetable_count(Count) :- !,
+        findall(X, (vegetable(X), all_ingredients(X)), L),
+        length(L, Count).
+
+vegetable("marchewka").
+vegetable("ziemniak").
+vegetable("cebula").
+vegetable("pomidor").
+vegetable("ogórek").
 
 
-recipe("bigos", ["kiełbasa", "kapusta"]).
-recipe("bigos ale lepszy", ["kiełbasa", "kapusta", "sól"]).
-recipe("bigos ale bez kiełbasy", ["kapusta"]).
+recipe("bigos") :- has_all_ingredients(["kiełbasa", "kapusta"]).
+recipe("rosół") :- mieso_count(M), M > 0, vegetable_count(C), C>0.
+recipe("schabowy z ziemiakami") :- has_all_ingredients(["schab", "ziemniak"]).
+recipe("spaghetti bolognese") :- has_all_ingredients(["mięso mielone", "pomidor", "olej", "czosnek"]).
+recipe("sałatka owocowa") :- fruits_count(C), C>1.
+recipe("sałatka warzywna") :- vegetable_count(C), C>2.
+
+recipe(X) :- custom_recipe(X, RequiredIngredients), has_all_ingredients(RequiredIngredients).
+
+get_recipes(X) :- recipe(X).
 
 :- dynamic(custom_recipe/2).
-
-recipe(X, Y) :- custom_recipe(X, Y).
-
-get_recipes(X, Ingredients) :- recipe(X, Required), has_all_ingredients(Required, Ingredients).
--- src/message_processor.py
+++ src/message_processor.py
@@ -12,29 +12,59 @@ class MessageProcessor:
         response = ""
         if message.startswith('+przepis'):
             response = self.add_recipe(message[len('+przepis'):])
-        elif message == 'lista':
-            response = self.list_recipes()
-        elif message.startswith('?przepis'):
-            response = self.get_recipes(message[len('?przepis'):])
+        elif message.startswith('+'):
+            response = self.add_ingredient(message[len('+'):])
+        elif message == 'czysc':
+            response = self.clear_ingredients()
+        elif message == 'owoce':
+            response = self.owoce()
+        elif message == '?przepisy':
+            response = self.get_recipes()
+        elif message == '?s':
+            response = self.get_ingredients()
         return True if response != "" else False, response
 
     def add_recipe(self, message):
         dish_name, products = message.split(':', 1)
         dish_name = dish_name.strip().lower()
         products = self.parse_products(products)
+        if list(self.prolog.query(f'custom_recipe("{dish_name}", {products})')):
+            return "Składnik został już dodany"
         self.prolog.assertz(f'custom_recipe("{dish_name}", {products})')
         return f"(Może) dodano przepis {dish_name}: {products}"
 
-    def get_recipes(self, message):
-        products = self.parse_products(message)
-        recipes = [output['X'] for output in self.prolog.query(f'get_recipes(X, {products})')]
+    def add_ingredient(self, message):
+        ingredient = message.strip().lower()
+        if list(self.prolog.query(f'ingredients("{ingredient}")')):
+            return "Składnik został już dodany"
+        self.prolog.assertz(f'ingredients("{ingredient}")')
+        return f"(Może) dodano składnik {ingredient}"
+
+    def clear_ingredients(self):
+        self.prolog.retractall(f'ingredients(_)')
+        return f"Wyczyszczono listę składników"
+
+    
+    def owoce(self):
+        fruits_count = list(self.prolog.query(f'fruits_count(X)'))[0]['X']
+        return str(fruits_count)
+
+    def get_recipes(self):
+        recipes = [output['X'] for output in self.prolog.query(f'get_recipes(X)')]
         recipes = [recipe if isinstance(recipe, str) else recipe.decode("utf-8") for recipe in recipes]
         if recipes is None or len(recipes) == 0:
             return "Co Pan zrobisz? Nic Pan z tego nie zrobisz"
         return "Dostępne przepisy:" + "".join([f'\n\t- {recipe}' for recipe in recipes])
 
+    def get_ingredients(self):
+        ingredients = [output['X'] for output in self.prolog.query(f'ingredients(X)')]
+        ingredients = [ingredient if isinstance(ingredient, str) else ingredient.decode("utf-8") for ingredient in ingredients]
+        if ingredients is None or len(ingredients) == 0:
+            return "Brak składników"
+        return "Składniki:" + "".join([f'\n\t- {ingredient}' for ingredient in ingredients])
+
     def list_recipes(self):
-        recipes = [output['X'] for output in self.prolog.query(f'get_recipes(X, Y)')]
+        recipes = [output['X'] for output in self.prolog.query(f'get_all_recipes(X)')]
         recipes = [recipe if isinstance(recipe, str) else recipe.decode("utf-8") for recipe in recipes]
         return "Lista wszystkich przepisów:" + "".join([f'\n\t- {recipe}' for recipe in recipes])
 
