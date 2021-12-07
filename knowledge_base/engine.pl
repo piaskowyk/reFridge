2adc75e1edea674ee38de0a73ccd338df4bbbb7f
Krzysztof Piaskowy <krzychu2956@gmail.com>
2021-12-07 23:46:11 +0100

    Przepisy (#2)

--- knowledge_base/engine.pl
+++ knowledge_base/engine.pl
@@ -1,1 +1,144 @@
-set_response(mleko).
+:- dynamic(ingredients/1).
+
+has_all_ingredients(RequiredIngredients) :-
+    forall(member(Element, RequiredIngredients), all_ingredients(Element)).
+
+has_any_ingredients(PossibleIngredients) :-
+        member(X, PossibleIngredients), all_ingredients(X).
+
+mieso_count(Count) :- !,
+        findall(X, (mieso(X), all_ingredients(X)), L),
+        length(L, Count).
+
+vegetable_count(Count) :- !,
+        findall(X, (vegetable(X), all_ingredients(X)), L),
+        length(L, Count).
+
+fruits_count(Count) :- !,
+        findall(X, (fruit(X), all_ingredients(X)), L),
+        length(L, Count).
+
+fruit("brzoskwinia").
+fruit("grusza").
+fruit("jabłko").
+fruit("morela ").
+fruit("śliwka").
+fruit("wiśnia ").
+fruit("czereśnie").
+fruit("cytryna").
+fruit("grejpfrut").
+fruit("mandarynka ").
+fruit("pomarańcza").
+fruit("figa").
+fruit("granat").
+fruit("mango").
+fruit("daktyle").
+fruit("kokos").
+fruit("awokado").
+fruit("malina").
+fruit("winogrono").
+fruit("agrest").
+fruit("aronia").
+fruit("borówk").
+fruit("jeżyna").
+fruit("malina").
+fruit("malinojeżyna").
+fruit("pigwa").
+fruit("porzeczka").
+fruit("żurawina").
+fruit("truskawka").
+fruit("ananas").
+fruit("banan").
+fruit("poziomka").
+fruit("truskawka").
+
+vegetable("bakłażan").
+vegetable("bób").
+vegetable("brokuł").
+vegetable("brukiew").
+vegetable("burak").
+vegetable("cebula").
+vegetable("cykoria").
+vegetable("czosnek").
+vegetable("dynia").
+vegetable("fasola").
+vegetable("groch").
+vegetable("jarmuż").
+vegetable("kalafior").
+vegetable("kalarepa").
+vegetable("kapusta").
+vegetable("karczoch").
+vegetable("koper").
+vegetable("kukurydza").
+vegetable("marchew").
+vegetable("ogórek").
+vegetable("papryka").
+vegetable("ziemniak").
+vegetable("pasternak").
+vegetable("pietruszka").
+vegetable("pomidor").
+vegetable("por").
+vegetable("roszponka").
+vegetable("rzepa").
+vegetable("rzeżucha").
+vegetable("rzodkiew").
+vegetable("rzodkiewka").
+vegetable("salsefia").
+vegetable("sałata").
+vegetable("seler").
+vegetable("skorzonera").
+vegetable("szalotka").
+vegetable("szczypiorek").
+vegetable("szpinak").
+
+mieso("wołowina").
+mieso("schab").
+mieso("cięlęcina").
+mieso("wieprzowina").
+mieso("drób").
+mieso("jagnięcina").
+mieso("dziczyzna").
+mieso("baranina").
+mieso("konina").
+mieso("parówki").
+
+all_ingredients(X) :- ingredients(X).
+all_ingredients("mięso mielone") :- has_any_ingredients(["wołowina", "wieprzowina"]).
+all_ingredients("wołowina") :- has_any_ingredients(["karkówka wołowa", "łopatka wołowa"]).
+all_ingredients("wieprzowina") :- has_any_ingredients(["schab", "polędwiczka", "szynka", "boczek"]).
+all_ingredients("drób") :- has_any_ingredients(["udka", "skrzydełka", "pierś z kurczaka"]).
+
+all_ingredients("dżem") :- has_any_ingredients(["marmolada", "dżem truskawkowy", "dżem malinowy", "powidło"]).
+all_ingredients("pieczywo") :- has_any_ingredients(["chleb", "bułka", "chleb tostowy", "chleb pszenny", "chleb razowy"]).
+
+recipe("bigos") :- has_all_ingredients(["kiełbasa", "kapusta"]).
+recipe("bitki") :- has_all_ingredients(["olej", "wołowina"]).
+recipe("rosół") :- mieso_count(M), M > 0, vegetable_count(C), C > 0.
+recipe("schabowy z ziemiakami") :- has_all_ingredients(["schab", "ziemniak"]).
+recipe("spaghetti bolognese") :- has_all_ingredients(["mięso mielone", "pomidor", "olej", "czosnek", "makaron"]).
+recipe("sałatka owocowa") :- fruits_count(C), C > 1.
+recipe("sałatka warzywna") :- vegetable_count(C), C > 2.
+recipe("sałatka warzywna z majonezem") :- vegetable_count(C), C>2, has_all_ingredients(["majonez"]).
+recipe("mięsne tornado") :- mieso_count(C), C > 2.
+recipe("burger") :- mieso_count(C), C > 0, has_all_ingredients(["olej"]), has_any_ingredients(["chleb", "chleb tostowy"]).
+recipe("basic kanapka") :- has_all_ingredients(["masło", "pieczywo"]), has_any_ingredients(["szynka", "ser"]).
+recipe("tosty") :- has_any_ingredients(["chleb", "bułka"]), has_any_ingredients(["ser"]).
+recipe("parówki w cieście") :- has_all_ingredients(["olej", "parówka", "mąka", "woda", "sól", "drożdże"]).
+recipe("placki ziemniaczane") :- has_all_ingredients(["olej", "ziemniaki", "mąka ziemniaczana", "woda", "sól"]).
+recipe("frytki") :- has_all_ingredients(["olej", "ziemniaki", "keczup", "sól"]).
+recipe("naleśniki") :- has_all_ingredients(["olej", "mąka", "mleko", "jajko"]).
+recipe("naleśniki z dżemem") :- has_all_ingredients(["olej", "mąka", "mleko", "jajko", "dżem"]).
+recipe("gofry") :- has_all_ingredients(["olej", "mąka", "mleko", "jajko"]).
+recipe("jajecznicza") :- has_all_ingredients(["olej", "jajko", "sól"]).
+recipe("jajecznicza mięsna") :- has_all_ingredients(["olej", "jajko", "sól"]), mieso_count(C), C > 0.
+recipe("jajecznicza ważywna") :- has_all_ingredients(["olej", "jajko", "sól"]), vegetable_count(C), C > 0.
+recipe("beza") :- has_all_ingredients(["olej", "jajko", "cukier"]).
+recipe("gorąca czekolada") :- has_all_ingredients(["mleko", "kakao", "cukier"]).
+recipe("kopiec kreta") :- has_all_ingredients(["mąka", "kakao", "cukier", "proszek do pieczenia", "mleko", "masło"]).
+recipe("kremówka") :- has_all_ingredients(["wafel", "margaryna"]).
+
+recipe(X) :- custom_recipe(X, RequiredIngredients), has_all_ingredients(RequiredIngredients).
+
+get_recipes(X) :- recipe(X).
+
+:- dynamic(custom_recipe/2).
--- src/message_processor.py
+++ src/message_processor.py
@@ -1,5 +1,5 @@
 from pyswip import Prolog
-
+import re
 
 class MessageProcessor:
     prolog = Prolog()
@@ -8,7 +8,61 @@ class MessageProcessor:
         self.prolog.consult('knowledge_base/engine.pl')
 
     async def parse(self, message_object, client) -> (bool, str):
-        output = ""
-        for response in self.prolog.query("set_response(Response)"):
-            output = output + str(response['Response'])
-        return True, output
+        message = message_object.content
+        response = ""
+        if message.startswith('+przepis'):
+            response = self.add_recipe(message[len('+przepis'):])
+        elif message.startswith('+'):
+            response = self.add_ingredient(message[len('+'):])
+        elif message == 'czysc':
+            response = self.clear_ingredients()
+        elif message == '?przepisy':
+            response = self.get_recipes()
+        elif message == '?s':
+            response = self.get_ingredients()
+        return True if response != "" else False, response
+
+    def add_recipe(self, message):
+        dish_name, products = message.split(':', 1)
+        dish_name = dish_name.strip().lower()
+        products = self.parse_products(products)
+        if list(self.prolog.query(f'custom_recipe("{dish_name}", {products})')):
+            return "Składnik został już dodany"
+        self.prolog.assertz(f'custom_recipe("{dish_name}", {products})')
+        return f"(Może) dodano przepis {dish_name}: {products}"
+
+    def add_ingredient(self, message):
+        ingredient = message.strip().lower()
+        if list(self.prolog.query(f'ingredients("{ingredient}")')):
+            return "Składnik został już dodany"
+        self.prolog.assertz(f'ingredients("{ingredient}")')
+        return f"(Może) dodano składnik {ingredient}"
+
+    def clear_ingredients(self):
+        self.prolog.retractall(f'ingredients(_)')
+        return f"Wyczyszczono listę składników"
+
+    def get_recipes(self):
+        recipes = [output['X'] for output in self.prolog.query(f'get_recipes(X)')]
+        recipes = [recipe if isinstance(recipe, str) else recipe.decode("utf-8") for recipe in recipes]
+        if recipes is None or len(recipes) == 0:
+            return "Co Pan zrobisz? Nic Pan z tego nie zrobisz"
+        return "Dostępne przepisy:" + "".join([f'\n\t- {recipe}' for recipe in recipes])
+
+    def get_ingredients(self):
+        ingredients = [output['X'] for output in self.prolog.query(f'ingredients(X)')]
+        ingredients = [ingredient if isinstance(ingredient, str) else ingredient.decode("utf-8") for ingredient in ingredients]
+        if ingredients is None or len(ingredients) == 0:
+            return "Brak składników"
+        return "Składniki:" + "".join([f'\n\t- {ingredient}' for ingredient in ingredients])
+
+    def list_recipes(self):
+        recipes = [output['X'] for output in self.prolog.query(f'get_all_recipes(X)')]
+        recipes = [recipe if isinstance(recipe, str) else recipe.decode("utf-8") for recipe in recipes]
+        return "Lista wszystkich przepisów:" + "".join([f'\n\t- {recipe}' for recipe in recipes])
+
+    def parse_products(self, products):
+        products = [product.strip().lower() for product in re.split(r"\s*[,.]+\s*", products)]
+        products = [f'"{product}"' for product in products]
+        products = "[" + ', '.join(products) + "]"
+        return products
